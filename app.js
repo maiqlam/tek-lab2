dd6a2561e52fb3f823bab417175ca67e84c5c384
maiqlam <maiqlam@gmail.com>
2023-05-29 19:24:13 -0500

    Methods Revisited answered

--- app.js
+++ app.js
@@ -62,16 +62,53 @@
 // console.log(wolfy);
 
 // ---------------- 5. Yell at the Ninja Turtles ----------------
-const ninjaTurtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"];
+// const ninjaTurtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"];
 
-for (const i of ninjaTurtles) {
-    console.log(i.toUpperCase());
-}
+// for (const i of ninjaTurtles) {
+//     console.log(i.toUpperCase());
+// }
 
-// ---------------- 6. Exercise ----------------
+// // ---------------- 6. Methods, Revisited  ----------------
+// const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
+// // Console log the index of Titanic.
+// console.log(favMovies.indexOf("Titanic")); // 8
 
+// // Use the .sort method.
+// favMovies.sort();
 
-// ---------------- 7. Exercise ----------------
+// // Use the method pop.
+// favMovies.pop();
+
+// // // push "Guardians of the Galaxy".
+// favMovies.push("Guardians of the Galaxy");
+
+// // // Reverse the array.
+// favMovies.reverse();
+
+// // // Use the shift method.
+// favMovies.shift();
+
+// // // unshift - what does it return?
+// favMovies.unshift("Interstellar");
+
+// // // splice "Django Unchained" and add "Avatar" (try finding the index of "Django Unchained" instead of counting it yourself).
+// favMovies.splice(favMovies.indexOf("Django Unchained"), 1, "Avatar");
+
+// // // slice the last half of the array. (Challenge yourself and try to programatically determine the middle of the array rather than counting it and hard coding it.)
+// let movieSlice = (favMovies.slice(favMovies.length/2, favMovies.length));
+
+// // // Store the value of your slice in a variable and console.log it.
+// console.log(movieSlice);
+
+// // // console.log your final results.
+// console.log(favMovies);
+
+// // // After running the above tasks, console.log the index of "Fast and Furious."
+// console.log(favMovies.indexOf("Fast and Furious"));
+// // We removed it from the array. What value do we get when we look for the index of something that is not in the array?
+// // index is -1 if not found in the array (but f+f was not removed, so index of f+f is 14).
+
+// ---------------- 7. Where is Waldo ----------------
 
 
 // ---------------- 8. Exercise ----------------
